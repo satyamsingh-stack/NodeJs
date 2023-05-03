55fa458ff4737acbcf397bb14955cb393cb103f8
Satyam Singh <>
2023-05-03 14:27:09 +0530

    Asynchronous Programming Language

---
+++ proof_of_Asynchronous/index.js
@@ -0,0 +1,7 @@
+console.log("Start exe..");
+
+setTimeout(()=>{
+    console.log("Logic exe");     //compiler do not wait for this colsole it will start end exc that is called as Asynchronous Programming Language
+},2000);
+
+console.log("End exe");
