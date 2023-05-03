0f24fb698e0d43550a7552acfe14e46bdfd15275
Satyam Singh <>
2023-05-03 16:49:14 +0530

    request from browser

--- starting_with_express/index.js
+++ starting_with_express/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('',(req,resp)=>{
-    resp.send("Welcome, to Home Page");
+app.get("/",(req,resp)=>{
+    console.log("Data Received from Browser is ->>>>>",req.query.name);
+    resp.send("Welcome, to Home Page"+req.query.name); // URL on the browser is: http://localhost:5000/?name=satyam
 })
 
 app.get('/About',(req,resp)=>{
