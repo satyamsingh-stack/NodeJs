38c616ee8dae8d2596455e70886301846ea9fda0
Shivam kumar <shivam.ss.kv@gmail.com>
2023-05-10 21:07:57 +0530

    as it is

--- Databases/connection/index.js
+++ Databases/connection/index.js
@@ -7,7 +7,7 @@ async function getdata(){
     let result = await client.connect();
     let db = result.db(database);
     let collection = db.collection('learning');
-    let response = await collection.find({"name":"Satyam Singh"}).toArray();
+    let response = await collection.find({}).toArray();
     console.log(response);
 }
 
